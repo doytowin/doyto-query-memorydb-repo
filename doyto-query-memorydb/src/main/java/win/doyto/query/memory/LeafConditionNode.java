d28dd998a22c1c1ce6e2cc8bda8aca2c485a1d7e
Forb Yuan <f0rb@163.com>
2024-07-21 16:43:43 +0800

    refactor condition display

--- doyto-query-memorydb/src/main/java/win/doyto/query/memory/LeafConditionNode.java
+++ doyto-query-memorydb/src/main/java/win/doyto/query/memory/LeafConditionNode.java
@@ -21,7 +21,7 @@ import win.doyto.query.core.QuerySuffix;
 
 import java.util.function.Predicate;
 
-import static win.doyto.query.core.QuerySuffix.resolve;
+import static win.doyto.query.core.QuerySuffix.*;
 import static win.doyto.query.util.CommonUtil.readField;
 
 /**
@@ -34,22 +34,29 @@ public class LeafConditionNode<E> implements ConditionNode<E> {
 
     private final String entityFieldName;
     private final Predicate<Object> delegate;
-    private final QuerySuffix querySuffix;
-    private final Object qfv;
+    private final String condition;
 
     public LeafConditionNode(String queryFieldName, Object queryFieldValue) {
-        this.querySuffix = resolve(queryFieldName);
+        QuerySuffix querySuffix = resolve(queryFieldName);
         this.entityFieldName = querySuffix.resolveColumnName(queryFieldName);
-        this.qfv = queryFieldValue;
-        this.delegate = FilterExecutor.build(querySuffix, qfv);
+        this.delegate = FilterExecutor.build(querySuffix, queryFieldValue);
+        this.condition = buildCondition(querySuffix, queryFieldValue);
+    }
+
+    private static String buildCondition(QuerySuffix querySuffix, Object queryFieldValue) {
+        if (querySuffix == Null) {
+            return Boolean.TRUE.equals(queryFieldValue) ? "== null" : "!= null";
+        }
+        String op = querySuffix == NONE ? "==" : querySuffix.name();
+        return op + " " + queryFieldValue;
     }
 
     @Override
     public boolean test(E entity) {
         Object entityFieldValue = readField(entity, entityFieldName);
         boolean result = delegate.test(entityFieldValue);
-        log.debug("Filtering for [{}.{}]: ({} {} {}) -> {}", entity.getClass().getSimpleName(),
-                entityFieldName, entityFieldValue, querySuffix, qfv, result);
+        log.debug("Filtering for [{}.{}]: ({} {}) -> {}", entity.getClass().getSimpleName(),
+                entityFieldName, entityFieldValue, condition, result);
         return result;
     }
 }
