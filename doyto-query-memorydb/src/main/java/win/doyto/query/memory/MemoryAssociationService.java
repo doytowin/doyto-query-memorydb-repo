73ea7241c6f8fe1c94da04fb78bcf0376ad9e96b
Forb Yuan <f0rb@163.com>
2024-08-16 14:37:29 +0800

    add AssociationService implementation

---
+++ doyto-query-memorydb/src/main/java/win/doyto/query/memory/MemoryAssociationService.java
@@ -0,0 +1,71 @@
+package win.doyto.query.memory;
+
+import win.doyto.query.core.AssociationService;
+import win.doyto.query.core.UniqueKey;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * MemoryAssociationService
+ *
+ * @author f0rb on 2024/8/12
+ */
+public class MemoryAssociationService<K1, K2> implements AssociationService<K1, K2> {
+    static final Map<UniqueKey<String, String>, Set<? extends UniqueKey<?, ?>>> associationMap = new ConcurrentHashMap<>();
+
+    private final Set<UniqueKey<K1, K2>> pairs;
+
+    public MemoryAssociationService(String k1Name, String k2Name) {
+        pairs = new LinkedHashSet<>();
+        associationMap.put(new UniqueKey<>(k1Name, k2Name), pairs);
+    }
+
+    @Override
+    public int associate(Set<UniqueKey<K1, K2>> uniqueKeys) {
+        int cnt = pairs.size();
+        pairs.addAll(uniqueKeys);
+        return pairs.size() - cnt;
+    }
+
+    @Override
+    public int dissociate(Set<UniqueKey<K1, K2>> uniqueKeys) {
+        int cnt = pairs.size();
+        pairs.removeAll(uniqueKeys);
+        return cnt - pairs.size();
+    }
+
+    @Override
+    public List<K1> queryK1ByK2(K2 k2) {
+        return pairs.stream().filter(pair -> pair.getK2().equals(k2))
+                    .map(UniqueKey::getK1).toList();
+    }
+
+    @Override
+    public List<K2> queryK2ByK1(K1 k1) {
+        return pairs.stream().filter(pair -> pair.getK1().equals(k1))
+                    .map(UniqueKey::getK2).toList();
+    }
+
+    @Override
+    public int deleteByK1(K1 k1) {
+        int cnt = pairs.size();
+        pairs.removeIf(pair -> pair.getK1().equals(k1));
+        return cnt - pairs.size();
+    }
+
+    @Override
+    public int deleteByK2(K2 k2) {
+        int cnt = pairs.size();
+        pairs.removeIf(pair -> pair.getK2().equals(k2));
+        return cnt - pairs.size();
+    }
+
+    @Override
+    public long count(Set<UniqueKey<K1, K2>> uniqueKeys) {
+        return pairs.stream().filter(uniqueKeys::contains).count();
+    }
+}
---
+++ doyto-query-memorydb/src/test/java/win/doyto/query/memory/MemoryAssociationServiceTest.java
@@ -0,0 +1,70 @@
+package win.doyto.query.memory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import win.doyto.query.core.UniqueKey;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * MemoryAssociationServiceTest
+ *
+ * @author f0rb on 2024/8/12
+ */
+class MemoryAssociationServiceTest {
+
+    private MemoryAssociationService<Long, Integer> userRoleAstService;
+
+    @BeforeEach
+    void setUp() {
+        userRoleAstService = new MemoryAssociationService<>("user", "role");
+        userRoleAstService.associate(1L, 1);
+        userRoleAstService.associate(1L, 2);
+        userRoleAstService.associate(1L, 3);
+        userRoleAstService.associate(2L, 2);
+        userRoleAstService.associate(2L, 4);
+    }
+
+    @Test
+    void dissociate() {
+        assertThat(userRoleAstService.dissociate(2L, 2)).isEqualTo(1);
+        assertThat(userRoleAstService.dissociate(2L, 5)).isZero();
+    }
+
+    @Test
+    void queryK1ByK2() {
+        List<Long> k1List = userRoleAstService.queryK1ByK2(2);
+        assertThat(k1List).containsExactly(1L, 2L);
+    }
+
+    @Test
+    void queryK2ByK1() {
+        List<Integer> k2List = userRoleAstService.queryK2ByK1(1L);
+        assertThat(k2List).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void deleteByK1() {
+        int count = userRoleAstService.deleteByK1(1L);
+        assertThat(count).isEqualTo(3);
+        assertThat(userRoleAstService.queryK2ByK1(1L)).isEmpty();
+    }
+
+    @Test
+    void deleteByK2() {
+        int count = userRoleAstService.deleteByK2(3);
+        assertThat(count).isEqualTo(1);
+        assertThat(userRoleAstService.queryK1ByK2(3)).isEmpty();
+    }
+
+    @Test
+    void count() {
+        Set<UniqueKey<Long, Integer>> uniqueKeys = userRoleAstService.buildUniqueKeys(1L, Arrays.asList(1, 4, 5));
+        long count = userRoleAstService.count(uniqueKeys);
+        assertThat(count).isEqualTo(1);
+    }
+}
